fe8906a3c7d17a8cb3ea450d6e78902907646e8e
chapetta <cha.petta@hotmail.com>
2023-09-13 20:14:53 -0300

    Começando a montar o componente table e adicionando a pagina wallter

---
+++ src/components/Table.tsx
@@ -0,0 +1,21 @@
+function Table() {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <td>Descrição</td>
+          <td>Tag</td>
+          <td>Método de pagamento</td>
+          <td>Valor</td>
+          <td>Moeda</td>
+          <td>Câmbio utilizado</td>
+          <td>Valor convertido</td>
+          <td>Moeda de conversão</td>
+          <td>Editar/Excluir</td>
+        </tr>
+      </thead>
+    </table>
+  );
+}
+
+export default Table;
